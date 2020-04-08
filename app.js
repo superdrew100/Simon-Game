cdae705c0b1e5758f4ec711f96f241bd1867fddb
Drew Thomasson <superdrew100@Gmail.com>
2020-04-08 10:39:36 -0400

    made a userButtonPress Array

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ const button2 = document.getElementById('button2')
 const button3 = document.getElementById('button3')
 const button4 = document.getElementById('button4')
 
+const userButtonArray = []
+
 button1.addEventListener('click', function () {
     console.log('button1 is pressed')
+    userButtonArray.push(1)
 })
 
 button2.addEventListener('click', function () {
     console.log('button2 is pressed')
+    userButtonArray.push(2)
 })
 
 button3.addEventListener('click', function () {
     console.log('button3 is pressed')
+    userButtonArray.push(3)
 })
 
 button4.addEventListener('click', function () {
     console.log('button4 is pressed')
+    userButtonArray.push(4)
 })
