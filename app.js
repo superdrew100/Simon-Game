c4c466276be14375839c3ca789803a8e9257ddfe
Drew Thomasson <superdrew100@Gmail.com>
2020-04-08 12:13:25 -0400

    created a start button for the game and a way for the computer to check whether a the arrays match up when a button is pressed

--- app.js
+++ app.js
@@ -5,23 +5,75 @@ const button3 = document.getElementById('button3')
 const button4 = document.getElementById('button4')
 
 const userButtonArray = []
+const computerPatternArray = [1, 2, 3]
+
+document.addEventListener('click', function() {
+    console.log("game has started")
+
+})
+
+if (userButtonArray.length === computerPatternArray.length) {
+    if (JSON.stringify(userButtonArray) === JSON.stringify(computerPatternArray)) {
+        console.log("Both strings match!")
+    }
+    if (JSON.stringify(userButtonArray) !== JSON.stringify(computerPatternArray)) {
+        console.log("Both strings do not match")
+    }
+}
+
 
 button1.addEventListener('click', function () {
     console.log('button1 is pressed')
     userButtonArray.push(1)
+
+    if (userButtonArray.length === computerPatternArray.length) {
+        if (JSON.stringify(userButtonArray) === JSON.stringify(computerPatternArray)) {
+            console.log("Both strings match!")
+        }
+        if (JSON.stringify(userButtonArray) !== JSON.stringify(computerPatternArray)) {
+            console.log("Both strings do not match")
+        }
+    }
 })
 
 button2.addEventListener('click', function () {
     console.log('button2 is pressed')
     userButtonArray.push(2)
+
+    if (userButtonArray.length === computerPatternArray.length) {
+        if (JSON.stringify(userButtonArray) === JSON.stringify(computerPatternArray)) {
+            console.log("Both strings match!")
+        }
+        if (JSON.stringify(userButtonArray) !== JSON.stringify(computerPatternArray)) {
+            console.log("Both strings do not match")
+        }
+    }
 })
 
 button3.addEventListener('click', function () {
     console.log('button3 is pressed')
     userButtonArray.push(3)
+
+    if (userButtonArray.length === computerPatternArray.length) {
+        if (JSON.stringify(userButtonArray) === JSON.stringify(computerPatternArray)) {
+            console.log("Both strings match!")
+        }
+        if (JSON.stringify(userButtonArray) !== JSON.stringify(computerPatternArray)) {
+            console.log("Both strings do not match")
+        }
+    }
 })
 
 button4.addEventListener('click', function () {
     console.log('button4 is pressed')
     userButtonArray.push(4)
+
+    if (userButtonArray.length === computerPatternArray.length) {
+        if (JSON.stringify(userButtonArray) === JSON.stringify(computerPatternArray)) {
+            console.log("Both strings match!")
+        }
+        if (JSON.stringify(userButtonArray) !== JSON.stringify(computerPatternArray)) {
+            console.log("Both strings do not match")
+        }
+    }
 })
