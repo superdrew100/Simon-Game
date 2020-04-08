1fa40542ba269f3c8c0ca5258754672137547c4d
Drew Thomasson <superdrew100@Gmail.com>
2020-04-08 10:28:58 -0400

    added 1-4 colored buttons that function ad buttons displaying on the consol which buttons were pressed

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("app.js is linked")
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const button3 = document.getElementById('button3')
+const button4 = document.getElementById('button4')
+
+button1.addEventListener('click', function () {
+    console.log('button1 is pressed')
+})
+
+button2.addEventListener('click', function () {
+    console.log('button2 is pressed')
+})
+
+button3.addEventListener('click', function () {
+    console.log('button3 is pressed')
+})
+
+button4.addEventListener('click', function () {
+    console.log('button4 is pressed')
+})
